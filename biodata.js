e5175e1a9c03cc9d1efc48dd43e3f04d5cfed91a
putrilidyabum <putrilidya@gmail.com>
2018-08-29 10:52:34 +0700

    biodata

---
+++ biodata.js
@@ -0,0 +1,15 @@
+let Biodata = {
+    name: "lidya putri agustina",
+    ages : 17,
+    class: 11,
+    adress:"Jl.Kirab remaja",
+    school:"smk bina mandiri multimedia",
+    hobbies:["reading", "swim", "cooking"]
+}
+    console.log(
+        "Nama saya" + Biodata.name + 
+        " \nsaya umur " + Biodata.ages + 
+        "\nsaya kelas " + Biodata.class +  
+        "\nrumah saya " + Biodata.adress + 
+        "\nsaya bersekolah di " + Biodata.school +   
+        "\nhoby saya adalah " +  Biodata.hobbies[2])
